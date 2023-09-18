522dd3b233b05bff38869f217f8f1a14a7dc729f
Erin Atkinson <atkin163@mist-login01.scinet.local>
2023-09-18 11:32:29 -0400

    Added function to sponge_layer.jl that returns forcing

--- src/sponge_layer.jl
+++ src/sponge_layer.jl
@@ -15,5 +15,12 @@ end
 @inline function sponge_layer_damping(simulation_parameters; σ=0.2, c=0.2, target=0)
     rate = c * simulation_parameters.N₀ / 2π
     mask=sponge_layer_damping_profile(simulation_parameters; σ)
-    Relaxation(; rate, mask, target)
+    return Relaxation(; rate, mask, target)
+end
+
+@inline function get_sponge_layer_forcing(simulation_parameters; σ=0.2, c=0.2)
+    (b, v) = get_filament_state(simulation_parameters; verbose=false)
+    damping = sponge_layer_damping(simulation_parameters; σ, c)
+    b_damping = sponge_layer_damping(simulation_parameters; σ, c, target=b)
+    return (; u=damping, v=damping, w=damping, b=b_damping)
 end
