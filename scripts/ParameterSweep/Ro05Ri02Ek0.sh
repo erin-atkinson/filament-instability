0fc385bc2aa48e3178462397bb1eec0fed160fb4
Erin Atkinson <atkin163@mist-login01.scinet.local>
2023-10-17 15:20:33 -0400

    Created scripts for parameter sweep with turbulence

---
+++ scripts/ParameterSweep/Ro05Ri02Ek0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=2:00:00
+#SBATCH --job-name=filament-instability-Ro05Ri02Ek0
+#SBATCH --output=../scratch/logs/filament-instability/Ro05Ri02Ek0.txt
+module load cuda/11.0.3
+
+cd ~/filament-instability
+
+./../julia-1.8.5/bin/julia -t 8 --project="env" -- src/simulation.jl secondarycirculation parameters/ParameterSweep/Ro05Ri02Ek0.txt 1 10 100 ../scratch/filament-instability/Ro05Ri02Ek0
---
+++ scripts/ParameterSweep/Ro1Ri06Ek1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=2:00:00
+#SBATCH --job-name=filament-instability-Ro1Ri06Ek1
+#SBATCH --output=../scratch/logs/filament-instability/Ro1Ri06Ek1.txt
+module load cuda/11.0.3
+
+cd ~/filament-instability
+
+./../julia-1.8.5/bin/julia -t 8 --project="env" -- src/simulation.jl secondarycirculation parameters/ParameterSweep/Ro1Ri06Ek1.txt 1 10 100 ../scratch/filament-instability/Ro1Ri06Ek1
