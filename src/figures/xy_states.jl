a3dbe2eb46553a460c1df98225d093d508cba4a6
Erin Atkinson <atkin163@mist-login01.scinet.local>
2024-01-12 15:49:32 -0500

    Created function for plotting multiple xy states

--- src/figures/xy_states.jl
+++ src/figures/xy_states.jl
@@ -4,65 +4,85 @@ using CairoMakie
 using Statistics
 using ImageFiltering: imfilter, Kernel.gaussian
 
-
-@inline function xz_state_figure(runname, n; resolution=(1000, 500))
+@inline function xy_state_data(runname, n, slice; σ=0, field="v")
     foldername = "../scratch/filament-instability/$runname"
     filename = "down_front_mean.jld2"
     paramfilename = "parameters.jld2"
-    frames, grid = jldopen("$foldername/$filename") do file
-        keys(file["timeseries/t"]), file["serialized/grid"]
-        end;
-    xᶜᵃᵃ = xnodes(Center, grid)
-    xᶠᵃᵃ = xnodes(Face, grid)
-    zᵃᵃᶜ = znodes(Center, grid)
-    zᵃᵃᶠ = znodes(Face, grid)
-    function ψᶜᶜᶜ(uᶠᶜᶜ, wᶜᶜᶠ, xᶜᵃᵃ, xᶠᵃᵃ, zᵃᵃᶜ, zᵃᵃᶠ)
-        # Integrate
-        Δzᵃᵃᶜ = reshape(diff(zᵃᵃᶠ), 1, length(zᵃᵃᶜ))
-        Δx = xᶠᵃᵃ[2] - xᶠᵃᵃ[1]
-        aᶠᶜᶜ = cumsum(uᶠᶜᶜ .* Δzᵃᵃᶜ; dims=3)
-
-        bᶜᶜᶠ = cumsum(wᶜᶜᶠ .* Δx; dims=1)
-        aᶜᶜᶜ = (circshift(aᶠᶜᶜ, (-1, 0)) .+ aᶠᶜᶜ) / 2
-        bᶜᶜᶜ = (bᶜᶜᶠ[:, 1:end-1] .+ bᶜᶜᶠ[:, 2:end]) ./ 2
-        return -aᶜᶜᶜ .+ bᶜᶜᶜ
+    
+    # Should sort by iteration
+    filename_iters = map(readdir("../scratch/filament-instability/$runname/checkpoints")) do f
+        m = match(r"checkpoint_iteration(\d+)\.jld2", f)
+        parse(Int, m[1])
     end
+    iteration = sort(filename_iters)[n]
+    checkpoint_filename = "../scratch/filament-instability/$runname/checkpoints/checkpoint_iteration$iteration.jld2"
+    
     sp = jldopen("$foldername/$paramfilename") do file
         file["parameters/simulation"]
     end
-    file = jldopen("$foldername/$filename")
-
-
-    frame = frames[n]
-
-
-    ts = [file["timeseries/t/$f"] for f in frames] .- 1
-    v = file["timeseries/v_dfm/$frame"][:, 1, :]
-
-    u = file["timeseries/u_dfm/$frame"][:, 1, :]
-    w = file["timeseries/w_dfm/$frame"][:, 1, :]
-
-    b = file["timeseries/b_dfm/$frame"][:, 1, :]
-    σ=0
-    # Get the secondary cirulation streamfunction
-    ψ = imfilter(ψᶜᶜᶜ(u, w, xᶜᵃᵃ, xᶠᵃᵃ, zᵃᵃᶜ, zᵃᵃᶠ), gaussian((σ, 0), (4σ+1, 5)), "circular")
-    # Vorticity
     
-    ζ = imfilter((circshift(v, (-1, 0)) .- circshift(v, (1, 0))) / (xᶜᵃᵃ[3] - xᶜᵃᵃ[1]), gaussian((σ, 0), (4σ+1, 5)), "circular")
+    file = jldopen(checkpoint_filename)
     
-    title = "Ro=$(round(sp.Ro; digits=1)), Ri=$(round(sp.Ri; digits=2)), t = $(round(ts[n]; digits=2))"
-
-    axis_kwargs = (; xlabel="x", ylabel="z", title, limits=(-5, 5, -0.12, 0))
-
-    fig = Figure(; resolution)
-    ax = Axis(fig[1, 1]; axis_kwargs...)
-
-    ht = heatmap!(ax, xᶜᵃᵃ, zᵃᵃᶜ, v; colormap=:balance, colorrange=(-5, 5))
-    contour!(ax, xᶜᵃᵃ, zᵃᵃᶜ, ψ; colormap=:BrBG_10, levels=range(-0.024, 0.024, 40), alpha=1, linewidth=1)
-    bstep = 1.875
-    brange = minimum(b):bstep:maximum(b)
-    contour!(ax, xᶜᵃᵃ, zᵃᵃᶜ, b; color=(:black, 0.5), levels=brange, linewidth=1)
-    Colorbar(fig[1, 2], ht, label=L"\langle ζ \rangle")
+    # Grid
+    grid = file["grid"]
+    xᶜᵃᵃ = xnodes(Center, grid)
+    xᶠᵃᵃ = xnodes(Face, grid)
+    yᵃᶜᵃ = ynodes(Center, grid)
+    zᵃᵃᶜ = znodes(Center, grid)
+    zᵃᵃᶠ = znodes(Face, grid)
+    z = zᵃᵃᶜ[slice]
+    
+    # We just need the v and b
+    v = file["$field/data"][4:end-3, 4:end-3, slice]
+    b = file["b/data"][4:end-3, 4:end-3, slice]
+    t = file["clock"].time
     close(file)
+    
+    figtitle = "Ro=$(round(sp.Ro; digits=1)), Ri=$(round(sp.Ri; digits=2)), z = $(round(z; digits=2))"
+    axtitle = "t = $(round(t; digits=2))"
+    
+    (v, b) = map([v, b]) do field
+        imfilter(field, gaussian((σ, σ)), "circular")
+    end
+    
+    return (; figtitle, axtitle, xs=xᶜᵃᵃ, ys=yᵃᶜᵃ, z, v, b)
+end
+
+@inline function xy_state!(layout_cell; axtitle, xs, ys, v, b, kwargs...)
+    # Create a single plot in layout_cell
+    axis_kwargs = (;
+        xlabel="x",
+        ylabel="y",
+        title=axtitle,
+        limits=(-5, 5, -5, 5))
+    
+    # The steps between contours
+    bstep = 1.875
+    
+    brange = minimum(b):bstep:maximum(b)
+    
+    ax = Axis(layout_cell; axis_kwargs...)
+    ht = heatmap!(ax, xs, ys, v; colormap=:balance, colorrange=(-5, 5))
+    contour!(ax, xs, ys, b; color=(:black, 0.5), levels=brange, linewidth=1)
+    return ht
+end
+
+@inline function xy_states(runname, ns, slice; resolution=(1000, 500), σ=16, field="v")
+    n_plots = length(ns)
+    plot_datas = xy_state_data.(runname, ns, slice; σ, field)
+    
+    fig = Figure(; resolution)
+    # Make each plot
+    hts = map(enumerate(plot_datas)) do (i, plot_data)
+        xy_state!(fig[1, i]; plot_data...)
+    end
+    # Add a colourbar
+    Colorbar(fig[1, n_plots+1], hts[1], label=L"%$field (x, y, z)")
+    # Add a figure title
+    Label(fig[0, :], plot_datas[1].figtitle)
+    # Clean up the figure
+    n_plots > 1 && hideydecorations!.(fig.content[2:n_plots])
+    colgap!(fig.layout, 15)
+    rowgap!(fig.layout, 5)
     return fig
 end
